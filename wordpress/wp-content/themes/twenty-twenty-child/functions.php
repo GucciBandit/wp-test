f2e19b23b0368021c040fa0751e5668c919cebea
GucciBandit <js.patriarch@protonmail.com>
2021-05-31 03:45:22 +0300

    Create custom post type

--- wordpress/wp-content/themes/twenty-twenty-child/functions.php
+++ wordpress/wp-content/themes/twenty-twenty-child/functions.php
@@ -29,4 +29,59 @@ function remove_admin_bar() {
         show_admin_bar(false);
     }
 }
+function create_posttype() {
+  register_post_type( 'products',
+      array(
+          'labels' => array(
+              'name' => __( 'Products' ),
+              'singular_name' => __( 'Product' )
+          ),
+          'supports' => array(
+'title',
+'editor',
+'excerpt',
+'custom-fields',
+'thumbnail',
+'page-attributes'
+),
+          'public' => true,
+          'has_archive' => true,
+          'rewrite' => array('slug' => 'products'),
+          'show_in_rest' => true,
+
+      )
+  );
+}
+
+add_action( 'init', 'create_posttype' );
+
+function global_notice_meta_box() {
+
+  add_meta_box(
+      'global-notice',
+      __( 'Global Notice', 'sitepoint' ),
+      'global_notice_meta_box_callback',
+      'products'
+  );
+}
+
+
+
+add_action( 'add_meta_boxes', 'global_notice_meta_box' );
+
+function global_notice_meta_box_callback( $post ) {
+
+  // Add a nonce field so we can check for it later.
+  wp_nonce_field( 'global_notice_nonce', 'global_notice_nonce' );
+
+  $value = get_post_meta( $post->ID, '_global_notice', true );
+
+  echo '<input type="number" name="thePrice" id="thePrice" value="' . esc_attr( $value ) . '">' ;
+}
+
+add_action( 'save_post', 'saving_the_metabox_data', 10, 3  );
+
+function saving_the_metabox_data ( $post_id, $post, $update ) {
+update_post_meta( $post_id, 'price', $_REQUEST['thePrice'] );
+}
 ?>
