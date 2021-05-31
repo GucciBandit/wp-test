7cdcd0ba05d3aa8a0e4c5d43f3bcd0b2463cdf52
GucciBandit <js.patriarch@protonmail.com>
2021-05-31 03:44:30 +0300

    Create user

--- wordpress/wp-content/themes/twenty-twenty-child/functions.php
+++ wordpress/wp-content/themes/twenty-twenty-child/functions.php
@@ -6,4 +6,21 @@ function tt_child_enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
 
+add_action('after_setup_theme', 'add_tcm_user');
+
+function add_tcm_user() {
+    $userdata = array(
+        'user_login'  =>  'tcm-editor',
+        'user_email'    =>  'harelw@tcmintrade.com',
+        'user_pass'   =>  '123456789',
+        'role'      => 'editor'
+    );
+
+    $user_id = wp_insert_user( $userdata ) ;
+
+    //On success
+    if (!is_wp_error( $user_id )) {
+       remove_admin_bar();
+    }
+}
 ?>
